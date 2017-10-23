12a2b0be66d6399738fe7cb2a7678b99b23efff1
Dhruv Sharma <dhruvsha1992@gmail.com>
2017-10-23 10:04:40 +0530

    added admin console

--- shoppersdeal-server/src/main/java/com/nexusdevs/shoppersdeal/server/controller/AdminController.java
+++ shoppersdeal-server/src/main/java/com/nexusdevs/shoppersdeal/server/controller/AdminController.java
@@ -1,5 +1,89 @@
 package com.nexusdevs.shoppersdeal.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.nexusdevs.shoppersdeal.server.dto.ConsoleUser;
+import com.nexusdevs.shoppersdeal.server.dto.UserSession;
+import com.nexusdevs.shoppersdeal.server.service.AdminService;
+
 public class AdminController {
-	//For console admin shift api from ConsoleController
+	
+	private static Logger logger= LoggerFactory.getLogger(AdminService.class);
+	
+	@Autowired
+	private AdminService adminService;
+	
+	
+	@RequestMapping(value = "/register" , method = RequestMethod.POST)
+	public String registerConsoleUser(@RequestBody String userObjStr) {
+		JsonObject JsonObject = new JsonObject();
+		JsonObject.addProperty("success", false);
+		JsonObject.addProperty("data", "Error to create user");
+		try {
+			ConsoleUser userObj = new Gson().fromJson(userObjStr, ConsoleUser.class);
+			JsonObject registerStatus = adminService.registerConsoleUser(userObj);
+			return registerStatus.toString();
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return JsonObject.toString();
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginConsoleUser(@RequestBody String loginObjStr) {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("data", "Invalid Username/Password");
+		jsonObject.addProperty("success", false);
+		try {
+			ConsoleUser user = new Gson().fromJson(loginObjStr, ConsoleUser.class);
+			JsonObject loginStatus = adminService.loginConsoleUser(user);
+			return loginStatus.toString();
+		}
+		catch(Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return jsonObject.toString();
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.POST)
+	public String logoutConsoleUser(@RequestBody String logoutObjStr) {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("data", "logout unsuccessful");
+		jsonObject.addProperty("success", false);
+		try {
+			UserSession userSession = new Gson().fromJson(logoutObjStr, UserSession.class);
+			JsonObject logoutStatus = adminService.logoutConsoleUser(userSession);
+			jsonObject.add("data", logoutStatus);
+			jsonObject.addProperty("success", true);
+			return jsonObject.toString();
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		return jsonObject.toString();
+	}
+	
+	@RequestMapping(value="/validate", method = RequestMethod.POST)
+	public String validateConsoleUser(@RequestBody String validateTokenStr) {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("data", "invalid token");
+		jsonObject.addProperty("success", false);
+		try{
+			UserSession userSession = new Gson().fromJson(validateTokenStr, UserSession.class);
+			JsonObject validateStatus = adminService.validateConsoleUser(userSession);
+			return validateStatus.toString();
+		}
+		catch (Exception e) {
+			logger.error(e.getMessage(),e);
+		}
+		return jsonObject.toString();
+	}
 }
--- shoppersdeal-server/src/main/java/com/nexusdevs/shoppersdeal/server/service/ConsoleService.java
+++ shoppersdeal-server/src/main/java/com/nexusdevs/shoppersdeal/server/service/ConsoleService.java
@@ -1,7 +1,6 @@
 package com.nexusdevs.shoppersdeal.server.service;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +12,9 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.nexusdevs.shoppersdeal.server.dto.Category;
-import com.nexusdevs.shoppersdeal.server.dto.ConsoleUser;
 import com.nexusdevs.shoppersdeal.server.dto.Products;
 import com.nexusdevs.shoppersdeal.server.dto.SubCategory;
-import com.nexusdevs.shoppersdeal.server.dto.UserSession;
+
 import com.nexusdevs.shoppersdeal.server.utils.JsonUtils;
 import com.nexusdevs.shoppersdeal.server.utils.StringUtils;
 
@@ -27,92 +25,6 @@ public class ConsoleService {
 	
 	@Autowired
 	private DAOService daoService;
-	
-	public JsonObject registerConsoleUser(ConsoleUser user) {
-		try {
-			ConsoleUser userStatus = daoService.getConsoleUserDetail(user.getEmail());
-			if (userStatus != null)
-				return JsonUtils.errorResponse("User Already Exists");
-			
-			user.setUserId(UUID.randomUUID().toString().replaceAll("-", ""));
-			user.setHp(StringUtils.getMD5Hash(user.getHp()));
-			user.setCreateTime(System.currentTimeMillis());
-			user.setUpdateTime(0);
-			
-			String createUserStatus = daoService.createConsoleUser(user);
-			return JsonUtils.successResponse(createUserStatus);
-		}
-		catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return JsonUtils.errorResponse("error");
-	}
-	
-	public JsonObject loginConsoleUser(ConsoleUser user) {
-		try {
-			ConsoleUser loginStatus = daoService.loginConsoleUser(user.getEmail(), StringUtils.getMD5Hash(user.getHp()));
-			if(loginStatus == null)
-				return JsonUtils.errorResponse("Invalid Username/Password");
-			
-			UserSession userSession = createSessionForConsole(loginStatus.getEmail());
-			
-			JsonObject JsonObject = new JsonObject();
-			JsonObject.addProperty("success", true);
-			JsonObject.addProperty("token", userSession.getToken());
-			JsonObject.addProperty("userId", loginStatus.getUserId());
-			
-			return JsonObject;
-		}
-		catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return JsonUtils.errorResponse("error");
-	}
-	
-	public JsonObject logoutConsoleUser(UserSession userSession) {
-		try {
-			daoService.logoutConsoleUser(userSession);
-			return JsonUtils.successResponse("logout successful");
-		}
-		catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return JsonUtils.errorResponse("error");
-	}
-	
-	public JsonObject validateConsoleUser(UserSession userSession) {
-		try {
-			UserSession validateStatus = daoService.validateConsoleUser(userSession);
-			if(validateStatus == null)
-				return JsonUtils.errorResponse("not valid");
-			
-			return JsonUtils.successResponse(validateStatus.toString());
-			
-		}
-		catch (Exception e) {
-			logger.error(e.getMessage(), e);
-		}
-		return JsonUtils.errorResponse("error");
-	}
-	
-	private UserSession createSessionForConsole(String email) {
-		long n = System.nanoTime() * System.nanoTime();
-		if (n <= 0) {
-			n = -n;
-		}
-		int k = (int) (email.hashCode() * Math.random());
-		if (k < 0)
-			k = -k;
-		String token = n + "" + k;
-		UserSession session = new UserSession();
-		session.setCreateTime(System.currentTimeMillis());
-		session.setToken(token);
-		session.setUserId(email);
-		daoService.insertConsoleSession(session);
-		return session;
-	}
-	
-	
 	
 	/*Category Start*/
 	public JsonArray categoryList(int n, int pos, String sF, String sT) {
